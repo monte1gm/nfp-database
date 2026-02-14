6a5b7aef07c305fc2a5c3090e6797338da9f388a
monte1gm <gabriel.montes@hotmail.com>
2026-02-13 21:12:27 -0600

    Updated for firebase rules

--- web/public/js/firebase.init.js
+++ web/public/js/firebase.init.js
@@ -3,12 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: 'TODO_FIREBASE_API_KEY',
-  authDomain: 'TODO_FIREBASE_AUTH_DOMAIN',
-  projectId: 'TODO_FIREBASE_PROJECT_ID',
-  storageBucket: 'TODO_FIREBASE_STORAGE_BUCKET',
-  messagingSenderId: 'TODO_FIREBASE_MESSAGING_SENDER_ID',
-  appId: 'TODO_FIREBASE_APP_ID'
+  apiKey: "AIzaSyAUnRUGVYGAndVYYdrGYtHPMiI3_dx7Zvc",
+  authDomain: "nfp-dataload.firebaseapp.com",
+  projectId: "nfp-dataload",
+  storageBucket: "nfp-dataload.firebasestorage.app",
+  messagingSenderId: "1011179966717",
+  appId: "1:1011179966717:web:693a7865bb35df14816e94",
+  measurementId: "G-QKN2NXKHL0"
 };
 
 export const app = initializeApp(firebaseConfig);
